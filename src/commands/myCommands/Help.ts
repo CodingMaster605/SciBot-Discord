7d3de941f6c67a169a2e2c03c82d21c5dbed1d53
Ibrahim <14irahman12345@gmail.com>
2020-06-23 10:13:25 +0400

    Edited Help Command From Mistake

--- src/commands/myCommands/Help.ts
+++ src/commands/myCommands/Help.ts
@@ -15,7 +15,7 @@ export default class HelpCommand extends Command {
     public async exec(message: Message): Promise<Message> {
         const embed = new MessageEmbed()
         .setTitle(`${this.client.user.username} | Help`)
-        .setDescription(`**ℹ Help:** Get a list of commands.\n  **Usage:** .help or .info\n\n**🏓 Ping:** Get the ping/latency of the bot.\n   **Usage:** .ping or .latency\n\n**🐱‍💻 Invite:** Get the bot\'s invite link\n   **Usage:** .invite\n\n**🧑 Avatar:** Find the Avatar of a specific member/user\n  **Usage:** .avatar or .getavatar\n  **Examples:** .avatar TreeBot or .avatar @TreeBot#5194\n\n💻 Host: Find out where TreeBot was hosted! \n Usage: .host or .bothost\n\nI hope this helped you... 😎`)
+        .setDescription(`**ℹ Help:** Get a list of commands.\n  **Usage:** .help or .info\n\n**🏓 Ping:** Get the ping/latency of the bot.\n   **Usage:** .ping or .latency\n\n**🐱‍💻 Invite:** Get the bot\'s invite link\n   **Usage:** .invite\n\n**🧑 Avatar:** Find the Avatar of a specific member/user\n  **Usage:** .avatar or .getavatar\n  **Examples:** .avatar TreeBot or .avatar @TreeBot#5194\n\n**💻 Host:** Find out where TreeBot was hosted! \n **Usage:** .host or .bothost\n\nI hope this helped you... 😎`)
         .setColor("RANDOM")
         return message.util.send(embed);
     }
