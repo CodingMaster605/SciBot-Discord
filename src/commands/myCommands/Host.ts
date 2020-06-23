ea5d7aeb6e1795786cf2580f1837e4ab38a84eb3
Ibrahim <14irahman12345@gmail.com>
2020-06-23 08:10:33 +0400

    Edit Mistake

--- src/commands/myCommands/Host.ts
+++ src/commands/myCommands/Host.ts
@@ -15,7 +15,7 @@ export default class HostCommand extends Command {
     public async exec(message: Message): Promise<Message> {
         const embed = new MessageEmbed()
         .setAuthor(`${this.client.user.username} | Bot Host`)
-        .setDescription(`Hey **${message.author.username}**!\n\nI was hosted by: DanHosting!\nWhcih was made by: DanTechBoy \n\n If you want to know DanTechBoy has a website! \n Click [here](https://www.dantechboy.com/) to go on it! \n\n I was created/programmed by: iMemez! \nHe has a YouTube Channel! Click [here](https://www.youtube.com/channel/UCGid_mQ8_0eG14l38ANYPAA/) to go on it! `)
+        .setDescription(`Hey **${message.author.username}**!\n\nI was hosted by: DanHosting!\n Which was made by: DanTechBoy \n\n If you want to know DanTechBoy has a website! \n Click [here](https://www.dantechboy.com/) to go on it! \n\n I was created/programmed by: iMemez! \nHe has a YouTube Channel! Click [here](https://www.youtube.com/channel/UCGid_mQ8_0eG14l38ANYPAA/) to go on it! `)
         .setColor("RANDOM")
         return message.util.send(embed);
     }
